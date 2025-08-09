50bf7cc66d9877c03b7cf2f1c168c963ef5c0bcc
Heron Sousa <heronrs1@gmail.com>
2025-08-09 00:55:26 -0300

    chore: add better-auth config

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,10 +1,52 @@
 import { relations } from "drizzle-orm";
-import { pgTable, uuid, text, integer, timestamp } from "drizzle-orm/pg-core";
+import { pgTable, uuid, text, integer, timestamp, boolean } from "drizzle-orm/pg-core";
 
 export const userTable = pgTable("user", {
-  id: uuid().primaryKey().defaultRandom(),
-  name: text().notNull(),
-})
+          id: text('id').primaryKey(),
+          name: text('name').notNull(),
+ email: text('email').notNull().unique(),
+ emailVerified: boolean('email_verified').$defaultFn(() => false).notNull(),
+ image: text('image'),
+ createdAt: timestamp('created_at').$defaultFn(() => /* @__PURE__ */ new Date()).notNull(),
+ updatedAt: timestamp('updated_at').$defaultFn(() => /* @__PURE__ */ new Date()).notNull()
+        });
+
+export const sessionTable = pgTable("session", {
+  id: text('id').primaryKey(),
+  expiresAt: timestamp('expires_at').notNull(),
+  token: text('token').notNull().unique(),
+  createdAt: timestamp('created_at').notNull(),
+  updatedAt: timestamp('updated_at').notNull(),
+  ipAddress: text('ip_address'),
+  userAgent: text('user_agent'),
+  userId: text('user_id').notNull().references(()=> userTable.id, { onDelete: 'cascade' })
+});
+
+export const accountTable = pgTable("account", {
+  id: text('id').primaryKey(),
+  accountId: text('account_id').notNull(),
+  providerId: text('provider_id').notNull(),
+  userId: text('user_id').notNull().references(()=> userTable.id, { onDelete: 'cascade' }),
+  accessToken: text('access_token'),
+  refreshToken: text('refresh_token'),
+  idToken: text('id_token'),
+  accessTokenExpiresAt: timestamp('access_token_expires_at'),
+  refreshTokenExpiresAt: timestamp('refresh_token_expires_at'),
+  scope: text('scope'),
+  password: text('password'),
+  createdAt: timestamp('created_at').notNull(),
+  updatedAt: timestamp('updated_at').notNull()
+});
+
+export const verificationTable = pgTable("verification", {
+  id: text('id').primaryKey(),
+  identifier: text('identifier').notNull(),
+  value: text('value').notNull(),
+  expiresAt: timestamp('expires_at').notNull(),
+  createdAt: timestamp('created_at').$defaultFn(() => /* @__PURE__ */ new Date()),
+  updatedAt: timestamp('updated_at').$defaultFn(() => /* @__PURE__ */ new Date())
+});
+
 
 export const productTable = pgTable("product", {
   id: uuid().primaryKey().defaultRandom(),
@@ -12,7 +54,7 @@ export const productTable = pgTable("product", {
   description: text().notNull(),
   slug: text().notNull().unique(),
   createdAt: timestamp("created_at").notNull().defaultNow(),
-  categoryId: uuid("category_id").notNull().references(() => categoryTable.id),
+  categoryId: uuid("category_id").notNull().references(() => categoryTable.id, { onDelete: 'set null' }),
 })
 
 export const categoryTable = pgTable("category", {
@@ -30,7 +72,7 @@ export const productVariantTable = pgTable("product_variant_table", {
   priceInCents: integer("price_in_cents").notNull(),
   slug: text().notNull().unique(),
   createdAt: timestamp("created_at").notNull().defaultNow(),
-  productId: uuid("product_id").notNull().references(() => productTable.id),
+  productId: uuid("product_id").notNull().references(() => productTable.id, { onDelete: 'cascade' }),
 })
 
 export const productVariantRelations = relations(productVariantTable, ({ one }) => ({
