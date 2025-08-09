89b33ca1469eccece17eaa57ef9139902185dda0
Heron Sousa <heronrs1@gmail.com>
2025-08-09 03:22:04 -0300

    feat: create products list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,46 @@
 import { Header } from "@/components/common/header";
+import ProductList from "@/components/common/product-list";
+import { db } from "@/db";
+import Image from "next/image";
 
-export default function Home() {
-  return <Header />;
+export default async function Home() {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
+
+  console.log(products);
+
+  return (
+    <>
+      <Header />
+
+      <div className="space-y-6">
+        <div className="px-5">
+          <Image
+            src="/banner-1.svg"
+            alt="Leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+
+        <ProductList products={products} title="Mais vendidos" />
+
+        <div className="px-5">
+          <Image
+            src="/banner-2.svg"
+            alt="Leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+      </div>
+    </>
+  );
 }
