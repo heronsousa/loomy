7930dc637eee1bc2110d55d4a74302f32d209e85
Heron Sousa <heronrs1@gmail.com>
2025-08-10 03:15:21 -0300

    feat: create cartItem

--- src/components/common/cart-menu.tsx
+++ src/components/common/cart-menu.tsx
@@ -11,6 +11,7 @@ import {
 } from "../ui/sheet";
 import { useQuery } from "@tanstack/react-query";
 import { getCart } from "@/actions/get-cart";
+import CartItem from "./cart-item";
 
 const CartMenu = () => {
   const { data: cart } = useQuery({
@@ -29,18 +30,17 @@ const CartMenu = () => {
         <SheetHeader>
           <SheetTitle>Carrinho</SheetTitle>
         </SheetHeader>
-        <div className="px-5">
-          {cart?.items.map(item => (
-            <div key={item.id} className="flex items-center justify-between py-2">
-              <div className="flex items-center gap-2">
-                <img src={item.productVariant.imageUrl} alt={item.productVariant.name} className="w-12 h-12 rounded" />
-                <div>
-                  <h4 className="font-semibold">{item.productVariant.name}</h4>
-                  <span className="text-sm text-muted-foreground">Quantidade: {item.quantity}</span>
-                </div>
-              </div>
-              <span className="font-semibold">R$ {item.productVariant.priceInCents / 100}</span>
-            </div>
+        <div className="space-y-4 px-5">
+          {cart?.items.map((item) => (
+            <CartItem
+              key={item.id}
+              id={item.id}
+              quantity={item.quantity}
+              productName={item.productVariant.product.name}
+              productVariantName={item.productVariant.name}
+              productVariantImageUrl={item.productVariant.imageUrl}
+              productVariantPriceInCents={item.productVariant.priceInCents}
+            />
           ))}
         </div>
       </SheetContent>
