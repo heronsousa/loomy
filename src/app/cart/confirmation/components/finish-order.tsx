47f869d0c8b980a9a9cb3a384add56c3a2583159
Heron Sousa <heronrs1@gmail.com>
2025-08-19 00:40:32 -0300

    feat: create cehckout session

--- src/app/cart/confirmation/components/finish-order.tsx
+++ src/app/cart/confirmation/components/finish-order.tsx
@@ -15,22 +15,43 @@ import {
 } from "@/components/ui/dialog";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { finishOrder } from '@/actions/finish-order'
+import { createCheckoutSession } from "@/actions/create-checkout-session";
+import { loadStripe } from "@stripe/stripe-js";
 
 const FinishOrder = () => {
   const [successDialogIsOpen, setSuccessDialogIsOpen] = useState(false);
 
   const queryClient = useQueryClient();
 
-  const { mutate: finishOrderMutation, isPending } = useMutation({
+  const { mutateAsync: finishOrderMutation, isPending } = useMutation({
     mutationKey: ["finish-order"],
-    mutationFn: () => finishOrder(),
+    mutationFn: async () => await finishOrder(),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["cart"] });
     },
   });
 
-  const handleFinishOrder = () => {
-    finishOrderMutation();
+  const handleFinishOrder = async () => {
+    if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
+      throw new Error("Stripe publishable key is not set");
+    }
+
+    const { orderId } = await finishOrderMutation();
+
+    const checkoutSession = await createCheckoutSession({ orderId });
+
+    const stripe = await loadStripe(
+      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+    );
+
+    if (!stripe) {
+      throw new Error("Failed to load Stripe");
+    }
+
+    await stripe.redirectToCheckout({
+      sessionId: checkoutSession.id,
+    });
+
     setSuccessDialogIsOpen(true);
   };
   return (
@@ -41,9 +62,7 @@ const FinishOrder = () => {
         onClick={handleFinishOrder}
         disabled={isPending}
       >
-        {isPending && (
-          <Loader2 className="h-4 w-4 animate-spin" />
-        )}
+        {isPending && <Loader2 className="h-4 w-4 animate-spin" />}
         Finalizar compra
       </Button>
       <Dialog open={successDialogIsOpen} onOpenChange={setSuccessDialogIsOpen}>
