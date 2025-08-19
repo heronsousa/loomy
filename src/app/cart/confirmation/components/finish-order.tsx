04cb26cdb871c59db5e18eef04c05994009caf84
Heron Sousa <heronrs1@gmail.com>
2025-08-19 01:09:50 -0300

    feat: create checkout sucess page

--- src/app/cart/confirmation/components/finish-order.tsx
+++ src/app/cart/confirmation/components/finish-order.tsx
@@ -1,26 +1,13 @@
 "use client";
 
 import { Loader2 } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
-import { useState } from "react";
-
 import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogTitle,
-} from "@/components/ui/dialog";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { finishOrder } from '@/actions/finish-order'
+import { finishOrder } from "@/actions/finish-order";
 import { createCheckoutSession } from "@/actions/create-checkout-session";
 import { loadStripe } from "@stripe/stripe-js";
 
 const FinishOrder = () => {
-  const [successDialogIsOpen, setSuccessDialogIsOpen] = useState(false);
-
   const queryClient = useQueryClient();
 
   const { mutateAsync: finishOrderMutation, isPending } = useMutation({
@@ -51,51 +38,17 @@ const FinishOrder = () => {
     await stripe.redirectToCheckout({
       sessionId: checkoutSession.id,
     });
-
-    setSuccessDialogIsOpen(true);
   };
   return (
-    <>
-      <Button
-        className="w-full rounded-full"
-        size="lg"
-        onClick={handleFinishOrder}
-        disabled={isPending}
-      >
-        {isPending && <Loader2 className="h-4 w-4 animate-spin" />}
-        Finalizar compra
-      </Button>
-      <Dialog open={successDialogIsOpen} onOpenChange={setSuccessDialogIsOpen}>
-        <DialogContent className="text-center">
-          <Image
-            src="/illustration.svg"
-            alt="Success"
-            width={300}
-            height={300}
-            className="mx-auto"
-          />
-          <DialogTitle className="mt-4 text-2xl">Pedido efetuado!</DialogTitle>
-          <DialogDescription className="font-medium">
-            Seu pedido foi efetuado com sucesso. Você pode acompanhar o status
-            na seção de “Meus Pedidos”.
-          </DialogDescription>
-
-          <DialogFooter>
-            <Button className="rounded-full" size="lg">
-              Ver meus pedidos
-            </Button>
-            <Button
-              className="rounded-full"
-              variant="outline"
-              size="lg"
-              asChild
-            >
-              <Link href="/">Voltar para a loja</Link>
-            </Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-    </>
+    <Button
+      className="w-full rounded-full"
+      size="lg"
+      onClick={handleFinishOrder}
+      disabled={isPending}
+    >
+      {isPending && <Loader2 className="h-4 w-4 animate-spin" />}
+      Finalizar compra
+    </Button>
   );
 };
 
---
+++ src/app/checkout/success/page.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+import { Header } from "@/components/common/header";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import Footer from "@/components/common/footer";
+import { useRouter } from "next/navigation";
+
+const CheckoutSuccessPage = () => {
+  const router = useRouter();
+
+  return (
+    <div className="flex flex-col justify-between h-screen">
+      <Header />
+
+      <Dialog open={true} onOpenChange={() => router.push("/")}>
+        <DialogContent className="text-center">
+          <Image
+            src="/illustration.svg"
+            alt="Success"
+            width={300}
+            height={300}
+            className="mx-auto"
+          />
+          <DialogTitle className="mt-4 text-2xl">Pedido efetuado!</DialogTitle>
+          <DialogDescription className="font-medium">
+            Seu pedido foi efetuado com sucesso. Você pode acompanhar o status
+            na seção de “Meus Pedidos”.
+          </DialogDescription>
+
+          <DialogFooter>
+            <Button className="rounded-full" size="lg">
+              Ver meus pedidos
+            </Button>
+            <Button
+              className="rounded-full"
+              variant="outline"
+              size="lg"
+              asChild
+            >
+              <Link href="/">Voltar para a loja</Link>
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default CheckoutSuccessPage;
